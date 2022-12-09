1cd501267a304621da6b9ef6ec23659bd7eac384
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2022-12-09 21:07:19 +0530

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', index, name='home'),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('edit_user/<str:username>', edit_user, name='edit_user'),
     path('user/change_password', change_password, name="change_password")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
